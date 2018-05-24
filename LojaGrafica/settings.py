6ed642622be486a6b929b56fc83c78b398d8b8a6
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-05-24 15:12:56 -0300

    fisrt

--- LojaGrafica/settings.py
+++ LojaGrafica/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
